9d525340f9578fce80116ea7c58a2bfaa426bf24
anacuende <113435331+anacuende@users.noreply.github.com>
2023-02-04 23:45:38 +0100

    Añadidos más endpoints

--- PicIt/webserviceapp/views.py
+++ PicIt/webserviceapp/views.py
@@ -78,3 +78,93 @@ def obtener_like(request):
 		diccionario['publicacion']= fila_sql.idPublic
 		respuesta_final.append(diccionario)
 	return JsonResponse(respuesta_final, safe = False)
+
+def crear_carpeta (request):
+	if request.method != ‘POST’:
+		return None
+	
+	json_peticion = json.loads(request.body)
+	folder = Folders()
+	folder.folder = json_peticion[‘nueva_carpeta’]
+	carpeta.publicacion = Publicaciones.objects.get(id = IdPublicacion)
+	carpeta.save()
+	return JsonResponse({“status”: “ok”})
+
+def mostrar_carpetas (request):
+	lista = folders.objects.all()
+	respuesta_final = []
+	for fila_sql in lista:
+		diccionario = {}
+		diccionario[‘Id’] = fila_sql.Id
+		diccionario[‘Name’] = fila_sql.Name
+		respuesta_final.append(diccionario)
+	return JsonResponse(respuesta_final, safe=False)
+
+def anadir_publicacion_carpeta (request, carpeta_id):
+	if request.method != ‘PATCH’:
+		return None
+	
+	json_peticion = json.loads(request.body)
+	post = Posts()
+	post.Posts = json_peticion[‘nueva_publicacion’]
+	carpeta.publicacion = Publicaciones.objects.get(id = IdPublicacion)
+	comentario.save()
+	return JsonResponse({“status”: “ok”})
+
+def mostrar_publicaciones_carpeta (request, id_solicitado):
+	carpeta = Folders.object.get(id = id_solicitado)
+	publicaciones = carpeta.Publicaciones_set.all()
+	lista_publicaciones = []
+	for fila_publicaciones_sql in publicaciones:
+		diccionario = {}
+		diccionario[‘Id’] = fila_publicaciones_sql.Id
+		diccionario[‘Imagen’] = fila_publicaciones_sql.Imagen
+		diccionario[‘Titulo’] = fila_publicaciones_sql.Titulo
+		diccionario[‘Descripción’] = fila_publicaciones_sql.Descripción
+		diccionario[‘Fecha’] = fila_publicaciones_sql.Fecha
+		lista_publicaciones.append(diccionario)
+	resultado = {
+		‘Id’: carpeta.Id,
+		‘Nombre’: carpeta.Nombre,
+		‘Publicaciones’: carpeta.lista_publicaciones
+	}
+	
+	return JsonResponse(resultado, json_dimps_params={‘ensure_ascii’: False})
+
+def listar_amigos (request):
+	lista = friends.objects.all()
+	respuesta_final = []
+	for fila_sql in lista:
+		diccionario = {}
+		diccionario[‘Username’] = fila_sql.Username
+		respuesta_final.append(diccionario)
+	return JsonResponse(respuesta_final, safe=False)
+
+def enviar_mensaje (request):
+	if request.method != ‘POST’:
+		return None
+	
+	json_peticion = json.loads(request.body)
+	message = Messages()
+	message.message = json_peticion[‘nuevo_mensaje’]
+	message.user = Publicaciones.objects.get(id = idUser)
+	message.save()
+	return JsonResponse({“status”: “ok”})
+
+def mostrar_mensajes_chat_concreto (request, id_solicitado):
+	usuario = Users.object.get(id = id_solicitado)
+	mensaje = usuario.Mensajes_set.all()
+	lista_mensajes = []
+	for fila_mensajes_sql in mensajes:
+		diccionario = {}
+		diccionario[‘Id’] = fila_publicaciones_sql.Id
+		diccionario[‘Mensaje’] = fila_publicaciones_sql.Mensaje
+		diccionario[‘Fecha’] = fila_publicaciones_sql.Fecha
+		lista_mensajes.append(diccionario)
+	resultado = {
+		‘Id’: carpeta.Id,
+		‘Username’: carpeta.Username,
+		‘Mensajes’: usuario.lista_mensajes
+	}
+	
+	return JsonResponse(resultado, json_dimps_params={‘ensure_ascii’: False})
